05ec72447729410a58e795751c3b25aa82fc3ad6
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-07-16 19:35:28 -0400

    ... nada funciona

--- src/main.f90
+++ src/main.f90
@@ -1,9 +1,13 @@
 program main
 ! begin of the program
+    USE ISO_FORTRAN_ENV
     implicit none
     ! declarations
     character(len=*), parameter :: input = 'hello world!'
     character (len = 20) :: first_name
+    character(len = 20) :: charArray
+    !
+    INTEGER :: i, n
     ! structure
     call log(input)
     ! my loop
@@ -12,7 +16,17 @@ program main
         write (*, '(a, 1x)', advance='no') '>'
         ! read input
         read (*, *) first_name 
-        call log(first_name)
+        !call log(first_name)
+
+        do i = 1, LEN_TRIM(first_name)
+            !
+            if (first_name(i:i) .NE. 'e') then  ! Check if character is not 'e'
+                WRITE(*, '(a)') TRIM(first_name(i:i)) !--> retorna cada um dos caracteres da string, menos os 'e'
+                charArray = charArray // TRIM(first_name(i:i))
+            endif
+            !
+        end do
+        WRITE(*,'(a)') charArray !--> retorna '��o�=R'
         ! end of the looop
     end do
 contains
