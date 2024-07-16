38fe14ca80ec64bf3a0e217c1e6a9b2d9a8e8b8f
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-07-16 16:01:51 -0400

    unicode finaly

--- src/main.f90
+++ src/main.f90
@@ -1,15 +1,28 @@
 program main
-!implicit none
-    ! declarations here
-    character (len = 15) :: first_name
-    !
-    write (*, '(a)') 'Hello world'
-    !
+! begin of the program
+    implicit none
+    ! declarations
+    character(len=*), parameter :: input = 'hello world!'
+    character (len = 20) :: first_name
+    ! structure
+    call log(input)
+    ! my loop
     do
-      write (*, '(a, 1x)', advance='no') '>'
-    
-      read (*, *) first_name 
-      write (*, '(a)') first_name
+        ! handle with the console
+        write (*, '(a, 1x)', advance='no') '>'
+        ! read input
+        read (*, *) first_name 
+        call log(first_name)
+        ! end of the looop
     end do
-
+contains
+    ! functions here
+    ! log values into the console
+    subroutine log(input)
+        character(len=*), intent(in) :: input
+        ! write the values into the console
+        write (*, '(a)') achar(27) // '[32m' // input // achar(27) // '[0m'
+        ! end of the function
+    end subroutine log
+! end of the program
 end program main
