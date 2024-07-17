54c655a950ed7942f55b0708b8d831b13db47eff
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-07-16 21:47:18 -0400

    ok

--- src/main.f90
+++ src/main.f90
@@ -3,36 +3,36 @@ program main
     USE ISO_FORTRAN_ENV
     implicit none
     ! declarations
-    character(len=*), parameter :: input = 'hello world!'
-    character (len = 20) :: first_name, charArray
+    character(len=*), parameter :: msg = 'hello world!'
+    character (len = 20) :: input, charArray
     !
     INTEGER :: i, n
     ! structure
-    call log(input)
+    call log(msg)
     ! my loop
     do
         ! handle with the console
         write (*, '(a, 1x)', advance='no') '>'
-        ! read input
-        read (*, *) first_name 
-        !call log(first_name)
+        ! read msg
+        read (*, *) input 
+        !call log(input)
 
-        first_name = TRIM(first_name)
+        input = TRIM(input)
         charArray = ''
         !
-        do i = 1, LEN_TRIM(first_name)
-            charArray = TRIM(charArray) // first_name(I:I)
+        do i = 1, LEN_TRIM(input)
+            charArray = TRIM(charArray) // input(I:I)
         end do
-        WRITE(*,'(A)') charArray
+        call log(charArray)
         ! end of the looop
     end do
 contains
     ! functions here
     ! log values into the console
-    subroutine log(input)
-        character(len=*), intent(in) :: input
+    subroutine log(msg)
+        character(len = *), intent(in) :: msg
         ! write the values into the console
-        write (*, '(a)') achar(27) // '[32m' // input // achar(27) // '[0m'
+        write (*, '(a)') achar(27) // '[32m' // msg // achar(27) // '[0m'
         ! end of the function
     end subroutine log
 ! end of the program
