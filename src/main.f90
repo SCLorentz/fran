64b928f7365cad9b04d45407c4022a3c292761ef
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-07-17 19:18:04 -0400

    .

--- src/main.f90
+++ src/main.f90
@@ -3,37 +3,55 @@ program main
     USE ISO_FORTRAN_ENV
     implicit none
     ! declarations
-    character(len=*), parameter :: msg = 'hello world!'
-    character (len = 20) :: input, charArray
+    character(len = *), parameter :: msg = 'hello world!'
+    character (len = 20) :: input, charArray, num_as_string
     !
     INTEGER :: i, n
     ! structure
-    call log(msg)
+    call write(msg)
     ! my loop
     do
         ! handle with the console
         write (*, '(a, 1x)', advance='no') '>'
         ! read msg
-        read (*, *) input 
-        !call log(input)
+        read (*, '(a)') input
+        !call write(input)
 
         input = TRIM(input)
         charArray = ''
         !
-        do i = 1, LEN_TRIM(input)
+        n = 0
+        do i = 1, len(input)
             charArray = TRIM(charArray) // input(I:I)
+            if (charArray(i:i) == 'f') then
+                n = n + 1
+            end if
         end do
-        call log(charArray)
+        !
+        if (n > 0) then
+            call write(to_string(n))
+        end if
+        !
+        call write(charArray)
         ! end of the looop
     end do
 contains
     ! functions here
-    ! log values into the console
-    subroutine log(msg)
+
+    ! write values into the console
+    subroutine write(msg)
+        ! declarations
         character(len = *), intent(in) :: msg
-        ! write the values into the console
+        ! write the value into the console
         write (*, '(a)') achar(27) // '[32m' // msg // achar(27) // '[0m'
         ! end of the function
-    end subroutine log
+    end subroutine write
+    
+    ! convert integer to string
+    function to_string(n) result(res)
+        integer, intent(in) :: n
+        character(len=10) :: res
+        write(res,'(i10)') n
+    end function to_string
 ! end of the program
 end program main
